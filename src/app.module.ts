597db2ff103f1ac46f6b74b28cf0a161a2df3c91
add-bakkers <add.re.0828@gmail.com>
2022-08-06 17:29:03 +0900

    add:docker env

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,16 @@ import { UsersModule } from './users/users.module';
 import { PlacesModule } from './places/places.module';
 import { PlansModule } from './plans/plans.module';
 import { PlanPlacesModule } from './plan-places/plan-places.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UsersModule, PlansModule, PlacesModule, PlanPlacesModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://tabilog_backend_mongodb_1:27017/tabilog'),
+    UsersModule,
+    PlansModule,
+    PlacesModule,
+    PlanPlacesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
