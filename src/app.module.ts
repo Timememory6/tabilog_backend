ab29f11b0cee710cd535cfddc9f6d7914a24c759
add-bakkers <add.re.0828@gmail.com>
2022-08-07 02:17:10 +0900

    googlemapsapi

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,12 @@ import { PlacesModule } from './places/places.module';
 import { PlansModule } from './plans/plans.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { RoutesModule } from './routes/routes.module';
+import { GoogleMapsModule } from './google-maps/google-maps.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://tabilog_backend_mongodb_1:27017/tabilog'),
+    GoogleMapsModule,
     UsersModule,
     PlansModule,
     PlacesModule,
---
+++ src/google-maps/google-maps.service.ts
@@ -0,0 +1,36 @@
+import { Client, LatLngLiteral } from '@googlemaps/google-maps-services-js';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class GoogleMapsService extends Client {
+  private readonly accessKey = this.config.get('GOOGLE_MAPS_ACCESS_KEY');
+
+  constructor(private config: ConfigService) {
+    super();
+  }
+
+  async getCoordinates(address: {
+    street: string;
+    number: string;
+    city: string;
+    state: string;
+    postalCode: string;
+  }): Promise<LatLngLiteral> {
+    const googleRes = await this.geocode({
+      params: {
+        address: `${address.street}, ${address.number}, ${address.city}, ${address.state}, ${address.postalCode}`,
+        key: this.accessKey,
+      },
+    });
+
+    const { lng, lat } = googleRes.data.results[0].geometry.location;
+    return { lng, lat };
+  }
+
+  async getAddressCoords(address): Promise<LatLngLiteral> {
+    const { lng, lat } = await this.getCoordinates(address);
+
+    return { lng, lat };
+  }
+}
